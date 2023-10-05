72102bdb0e26b3c92a3b83d7bd8488c6cd9d02d5
Jhens Salazar <3227147025j@gmail.com>
2023-10-05 02:17:46 -0500

    modal ver mas

--- assets/js/modal_carrito.js
+++ assets/js/modal_carrito.js
@@ -7,3 +7,13 @@ function cerrar() {
     document.getElementById("vent").style.display = "none";
     document.body.style.overflow = "auto"; // Reactiva el scroll en el body
 }
+
+function abrir1() {
+    document.getElementById("vent3").style.display = "block";
+}
+
+
+function cerrar1() {
+    document.getElementById("vent3").style.display = "none";
+    document.body.style.overflow = "auto"; // Reactiva el scroll en el body
+}
