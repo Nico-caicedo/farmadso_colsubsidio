83082739618ca61f1d145d8cd6607453f95814d5
David Mejia <mejia018000@gmail.com>
2023-10-05 01:25:30 -0500

    nuevo cambio

--- assets/js/modal_carrito.js
+++ assets/js/modal_carrito.js
@@ -1,7 +1,9 @@
 function abrir() {
     document.getElementById("vent").style.display = "block";
     document.body.style.overflow = "hidden"; // Desactiva el scroll en el body
+    window.scrollTo(0, 0); // Mueve el scroll al principio
 }
+
 
 function cerrar() {
     document.getElementById("vent").style.display = "none";
